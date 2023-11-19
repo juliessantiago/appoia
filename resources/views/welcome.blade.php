f9850db4dc74eb326f28c74e05e69752c7e4cb52
juliesantiago <santiago.sjulie@gmail.com>
2023-11-19 17:34:07 -0300

    Adiciona pacote do ScrollReveal

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,9 +4,15 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Appoia</title>
+  <!--CSS-->
   <link rel="stylesheet" href="style.css">
+  <!--swiper-->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+
 </head>
 <body>
   
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+<!-- <script src="https://unpkg.com/scrollreveal"></script> -->
 </body>
 </html>
