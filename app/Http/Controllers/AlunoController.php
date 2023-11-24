7bf7ab57cff85ef7b12fa23d4fcb036e8bf254de
juliesantiago <santiago.sjulie@gmail.com>
2023-11-24 17:04:27 -0300

    Ajusta tela alunos

--- app/Http/Controllers/AlunoController.php
+++ app/Http/Controllers/AlunoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Aluno;
 
 class AlunoController extends Controller
 {
---
+++ resources/views/alunos.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Alunos</title>
+</head>
+<body>
+
+    {{$alunos}}
+    <h1>Lista de Alunos</h1>
+
+    <ul>
+        @foreach($alunos as $aluno)
+            <li>{{ $aluno->nome }}</li>
+            <!-- Adicione outros campos que você deseja exibir -->
+        @endforeach
+    </ul>
+
+</body>
+</html>
