ea2a30920a74e717bf7a89eaeacd7c58f6d2aa68
gillgonzales <gillgonzales@ifsul.edu.br>
2023-12-04 18:04:30 -0300

    fix: force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -17,8 +18,9 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot(UrlGenerator $url): void
     {
-        //
+        if(app()->isProduction())
+        $url->forceScheme('https');
     }
 }
