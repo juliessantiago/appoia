8d021c8758fe2b631cbe1b683ba74f9fb4b71272
juliesantiago <santiago.sjulie@gmail.com>
2023-12-04 13:55:25 -0300

    Altera cor primária de painel admin do filament

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -31,8 +31,9 @@ class AdminPanelProvider extends PanelProvider
             ->login()
             ->registration()
             ->colors([
-                'primary' => Color::Sky,
+                'primary' =>'#E2D2F6',
             ])
+            ->font(family: 'Spartan') //não está funcionando... 
             ->sidebarCollapsibleOnDesktop()
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
