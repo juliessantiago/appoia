4a2f94d1b0e3e89f40822e4571b17ad5e8b6e0c7
juliesantiago <santiago.sjulie@gmail.com>
2023-12-04 15:31:46 -0300

    Altera logo do dashboard

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,14 +6,15 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                       <img src="{{ asset('images/logo_provisorio.png') }}" class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200">
+                        {{-- <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" /> --}}
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                        {{ __('Appoia') }}
                     </x-nav-link>
                 </div>
             </div>
