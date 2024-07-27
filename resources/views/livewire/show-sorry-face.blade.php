175e55c403147b5c16251048b3e2c04f18c98f23
juliesantiago <santiago.sjulie@gmail.com>
2024-07-27 15:19:54 -0300

    Adiciona mensagem de erro se não há voluntários

--- resources/views/livewire/show-sorry-face.blade.php
+++ resources/views/livewire/show-sorry-face.blade.php
@@ -1,7 +1,6 @@
 <div class="flex justify-center">
-    <div class="animate-bounce ">
-        <img  src="{{ asset('images/stickers/sorry.png')}}" alt="Desculpe imagem" class="w-2/3" />
-        <span class="text-3xl text-pink-400 font-bold text-center">Desculpe...</span>
+    <div class="animate-bounce flex flex-col justify-items-center content-start">
+        <img  src="{{ asset('images/stickers/sorry.png')}}" alt="Desculpe imagem" class="" />
     </div>
    
 </div>
