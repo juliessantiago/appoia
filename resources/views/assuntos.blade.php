01d4ab2867c1f1209dcee09f70e268d03be42062
juliesantiago <santiago.sjulie@gmail.com>
2024-07-27 15:42:40 -0300

    Ajusta página de "Assuntos"

--- resources/views/assuntos.blade.php
+++ resources/views/assuntos.blade.php
@@ -1,11 +1,14 @@
 <x-app-layout>
-    <div class="flex justify-center m-8">
-      <div class="flex-column max-w-md p-4 m-4 bg-purple-300 rounded-md">
+    <div class="flex justify-center content-center m-8">
+      <div class="flex-column max-w-md p-4 m-4 bg-sky-300 rounded-md">
         {{-- <img src="{{ asset('images/stickers/duvida.png')}}"/> --}}
         <h4 class="text-slate-50">Não sabe o que fazer agora? Tudo bem, vamos te ajudar!</h4>
         <span class="text-slate-50 text-md">Você pode começar escolhendo um assunto que gostaria de falar ou ir direto para a </span>
-        <span class="text-md text-slate-50 text-md mx-2 underline"><a href="{{route('allVoluntarios')}}">lista de voluntários</a></span>
+        <span class="text-2xl  text-slate-50 text-md mx-2 underline"><a href="{{route('allVoluntarios')}}">lista de voluntários</a></span>
       </div>
+      {{-- <div class=" md:mt-6">
+        <a href="{{route('allVoluntarios')}}" class="inline-flex items-center px-6 py-2 text-sm font-medium text-center text-white bg-purple-300 rounded-lg hover:bg-purple-400 focus:ring-4 focus:outline-none focus:ring-purple-400">Voluntários</a>
+      </div> --}}
     </div>
    
     @if($assuntos->count() > 0)
@@ -23,26 +26,21 @@
                 </div>
               </a><!--cardzinho-->
           @endforeach  --}}
-          
-        </div>
-
-      </div>
-    
-      <div class="flex justify-between px-14 py-6"> 
-        <div class="flex mt-4 md:mt-6"> <!--url()->previous() com problema --> 
-          <a href="{{url()->previous()}}"><img src="{{ asset('images/icons/voltar.png')}}" /></a> 
-          {{-- <a href="{{back()}}" class="inline-flex items-center px-6 py-2 text-sm font-medium text-center text-white bg-purple-300 rounded-lg hover:bg-purple-400 focus:ring-4 focus:outline-none focus:ring-purple-400">Sair </a> --}}
-        </div>
-        <div class="flex mt-4 md:mt-6">
-            <a href="{{route('voluntario.logout')}}" class="inline-flex items-center px-6 py-2 text-sm font-medium text-center text-white bg-purple-300 rounded-lg hover:bg-purple-400 focus:ring-4 focus:outline-none focus:ring-purple-400">Sair </a>
         </div>
       </div>
     @else
-    
     @livewire('show-sorry-face')
       <p class="text-md text-gray-400 text-center">Desculpe, ainda não temos nenhum assunto cadastrado.</p>
-      
       <p class="text-md text-gray-400 text-center"> Acesse a página com todos os voluntários</p>
     @endif 
+    <div class="flex justify-between px-14 py-6"> 
+      <div class="flex mt-4 md:mt-6"> <!--url()->previous() com problema --> 
+        <a href="{{url()->previous()}}"><img src="{{ asset('images/icons/voltar.png')}}" /></a> 
+        {{-- <a href="{{back()}}" class="inline-flex items-center px-6 py-2 text-sm font-medium text-center text-white bg-purple-300 rounded-lg hover:bg-purple-400 focus:ring-4 focus:outline-none focus:ring-purple-400">Sair </a> --}}
+      </div>
+      <div class="flex mt-4 md:mt-6">
+          <a href="{{route('voluntario.logout')}}" class="inline-flex items-center px-6 py-2 text-sm font-medium text-center text-white bg-purple-300 rounded-lg hover:bg-purple-400 focus:ring-4 focus:outline-none focus:ring-purple-400">Sair </a>
+      </div>
+    </div>
   </x-app-layout>
   
